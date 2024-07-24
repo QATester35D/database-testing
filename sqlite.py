29e319b6c82a4179a077ee993797825935764397
Shawn LoPorto <sloporto@asi-test.com>
2024-07-24 15:16:17 -0600

    Adding additional SQL statements

--- sqlite.py
+++ sqlite.py
@@ -1,36 +1,45 @@
-from sqllite3tools1 import dataBase
+# from sqllite3tools1 import dataBase
 import sqlite3
 import pathlib
 
+################################################
+# Database connection to a SQLite database
+################################################
 db_path="C:\\Users\\shawn\\AppData\\Roaming\\DBeaverData\\workspace6\\.metadata\\sample-database-sqlite-1\\Chinook.db"
 connection = sqlite3.connect(db_path)
-# connection = sqlite3.connect("{db_path}?mode=rw", uri=True) #this didn't work; probably need pathlib.path
 cursor = connection.cursor()
+
+################################################
+# Standard SQL statement
+################################################
 # cursor.execute("SELECT * from Album")
 cursor.execute("SELECT * from Album WHERE Title Like 'Black Sabbath%' ")
 result = cursor.fetchall()
-print ("result")
+print ("check debugger for values")
 
+################################################
+# Join statement
+################################################
 cursor.execute("SELECT Title "
                "FROM Artist a "
                 "JOIN Album a2 on a.ArtistId = a2.ArtistId "
                 "WHERE a.Name = 'AC/DC' "
                 "LIMIT 10")
 result = cursor.fetchall()
-print ("result")
+print ("check debugger for values")
 
+################################################
+# Left Join statement
+################################################
+cursor.execute("SELECT Customer.FirstName, Customer.LastName,CustomerId "
+               "FROM Customer "
+                "Left JOIN Invoice "
+                "Using (CustomerId) "
+                "WHERE Customer.LastName = 'Gonçalves'")
+nbrOfOrdersByCustomerId=len(cursor.fetchall()) #getting the number of values returned by the SQL statement
+print(f"Number of Orders by CustomerId is: {nbrOfOrdersByCustomerId}") #using f-string formatting
+
+################################################
+# Close the database connection when done
+################################################
 connection.close()
-
-# db=dataBase('path') #by default main.db
-# db.create_table('Students',id='INTEGER PRIMARY KEY AUTOINCREMENT',name='varchar(255)',email="varchar(255)")
-# #create_table(table_name:str,**kwargs) 
-# db.info('Students')
-# '|| 0, id, INTEGER, 0, None, 1 ||'
-# '|| 1, name, varchar(255), 0, None, 0 ||'
-# '|| 2, email, varchar(255), 0, None, 0 ||'
-# #info(table_name:str) return table info,cols,data types
-# db.add_to_table('Students',data1) #parametrs: <table_name:str>,<data:dict>
-# 'Executing:'
-# "INSERT INTO Users5('name', 'email') VALUES('John','John@gmail.com')"
-# # db.drop_table('Studets') #drop_table(table_name:str)
-
