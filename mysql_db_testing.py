a1a3855cb8bc3abef7d30176222bc8bd988c6800
Shawn LoPorto <sloporto@asi-test.com>
2024-07-25 15:40:40 -0600

    More updates

--- mysql_db_testing.py
+++ mysql_db_testing.py
@@ -12,11 +12,29 @@ mydb = mysql.connector.connect(user='root', password='root', database='sakila')
 cur = mydb.cursor()
 
 ################################################
-# Standard SQL statement
+# Standard SQL statements
 ################################################
 cur.execute("SELECT * from sakila.city")
 result = cur.fetchall()
 print ("check debugger for values")
+
+# Get the count for the rental table
+cur.execute("select count(rental_id) from sakila.rental")
+result = cur.fetchall()
+print (f"The total record count for the Rental table is: {result[0][0]}")
+
+# Get the average payment amount from the payment table for Mary Smith
+cur.execute("select avg(amount) from sakila.payment where customer_id = '1'")
+result = cur.fetchall()
+print (f"The average amount spent by Mary Smith before formatting is: {result[0][0]}")
+currency_string = '${:,.2f}'.format(result[0][0])
+print (f"The formated amount spent is now: {currency_string}")
+
+# Get the total amount spent by Mary Smith
+cur.execute("select sum(amount) from sakila.payment where customer_id = '1'")
+result = cur.fetchall()
+currency_string = '${:,.2f}'.format(result[0][0])
+print (f"The total amount spent by Mary Smith is: {currency_string}")
 
 #############################################################################
 # Standard SQL statement getting a specific address by city_id - Kansas City
@@ -45,9 +63,9 @@ cur.execute("SELECT address from sakila.address where address.city_id = '300'")
 result = cur.fetchall()
 print ("check debugger for values")
 
-################################################
-# Inner Join statement on the city Lethbridge from Alberta
-################################################
+##################################################################################
+# Inner Join statement on the city Lethbridge - matching city_id from both tables
+##################################################################################
 cur.execute("SELECT address.address, city.city "
                "FROM address "
                 "INNER JOIN city on address.city_id = city.city_id "
@@ -56,11 +74,16 @@ result = cur.fetchall()
 print ("check debugger for values")
 
 ################################################
-# Left Join statement
+# Left Join statement - Returns all records from the left table, and the matched records from the right table
 ################################################
-
+cur.execute("SELECT customer.first_name, customer.last_name, payment.payment_id, payment.amount "
+               "FROM sakila.customer "
+                "LEFT JOIN sakila.payment ON customer.customer_id = payment.customer_id "
+                "ORDER BY customer.last_name ")
+result = cur.fetchall()
+print ("check debugger for values")
 
 ################################################
 # Close the database connection when done
 ################################################
-connection.close()
+mydb.close()
